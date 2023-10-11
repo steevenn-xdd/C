bc9e9e380c5fabb15a5194c5accd7f1cfcc08fde
Steven N. Duarte C <60182916+steevenn-xdd@users.noreply.github.com>
2023-10-11 17:35:52 -0400

    11/10/2023

--- DiaSemana.c
+++ DiaSemana.c
@@ -1,37 +1,87 @@
-/*Escriba un programa en el que se pida ingresar numeros del 1 al 7 segun el numero elegido va a representar un dia de la semana*/
-
 #include <stdio.h>
-int main(){
-    int dia;
-    printf("Ingrese un numero para indicar su dia: ");
-    scanf("%d",&dia);
-    
-    switch (dia)
-    {
-    case 1:
-        printf("Hoy es Domingo dia %d de la semana ", dia); 
-        break;
-    case 2:
-        printf("Hoy es Lunes dia %d de la semana ", dia); 
-        break;
-    case 3:
-        printf("Hoy es Martes dia %d de la semana ", dia); 
-        break;
-    case 4:
-        printf("Hoy es Miercoles dia %d de la semana ", dia); 
-        break;
-    case 5:
-        printf("Hoy es Jueves dia %d de la semana ", dia); 
-        break;
-    case 6:
-        printf("Hoy es Viernes dia %d de la semana ", dia); 
-        break;
-    case 7:
-        printf("Hoy es Sabado dia %d de la semana ", dia); 
-        break;
-    
-    default:
-        break;
+
+int main() {
+    int opcion;
+
+    printf("Seleccione una opción (1 o 2):\n");
+    printf("1. Ver día de la semana\n");
+    printf("2. Realizar operaciones matemáticas\n");
+    scanf("%d", &opcion);
+
+    switch (opcion) {
+        case 1:
+            int dia;
+
+            printf("Ingresa un número (1-7) para conocer el día de la semana: ");
+            scanf("%d", &dia);
+
+            switch (dia) {
+                case 1:
+                    printf("Lunes\n");
+                    break;
+                case 2:
+                    printf("Martes\n");
+                    break;
+                case 3:
+                    printf("Miércoles\n");
+                    break;
+                case 4:
+                    printf("Jueves\n");
+                    break;
+                case 5:
+                    printf("Viernes\n");
+                    break;
+                case 6:
+                    printf("Sábado\n");
+                    break;
+                case 7:
+                    printf("Domingo\n");
+                    break;
+                default:
+                    printf("Número no válido, introduce un número entre 1 y 7.\n");
+                    break;
+            }
+            break;
+
+        case 2:
+            char operador;
+            double num1, num2, resultado;
+
+            printf("Ingresa un operador (+, -, *, /): ");
+            scanf(" %c", &operador);
+
+            printf("Ingresa dos números: ");
+            scanf("%lf %lf", &num1, &num2);
+
+            switch (operador) {
+                case '+':
+                    resultado = num1 + num2;
+                    break;
+                case '-':
+                    resultado = num1 - num2;
+                    break;
+                case '*':
+                    resultado = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        resultado = num1 / num2;
+                    } else {
+                        printf("No se puede dividir por cero.\n");
+                        break;
+                    }
+                    break;
+                default:
+                    printf("Operador no válido.\n");
+                    break;
+            }
+
+            printf("Resultado: %.2lf %c %.2lf = %.2lf\n", num1, operador, num2, resultado);
+            break;
+
+        default:
+            printf("Opción no válida. Debe seleccionar 1 o 2.\n");
+            break;
     }
 
     return 0;
